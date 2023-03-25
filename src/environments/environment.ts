47be120f22abb8b0870c628c52180d6c9d2dec16
kphutiyagae <kopanophutiyagae@gmail.com>
2023-03-25 14:17:11 +0200

    feat(none): :sparkles: Added Firebase configs.

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'vacationvault-f9e86',
+    appId: '1:210068870240:web:1f70d5e54b054bc2199f05',
+    storageBucket: 'vacationvault-f9e86.appspot.com',
+    apiKey: 'AIzaSyC9sN3cdQc_jZRKAHvzgjCLrhbsC_rZEYY',
+    authDomain: 'vacationvault-f9e86.firebaseapp.com',
+    messagingSenderId: '210068870240',
+  }
+}
