0a7198546be84f2f828f20838f42aa268854d94a
kphutiyagae <kopanophutiyagae@gmail.com>
2023-03-27 09:05:48 +0200

    refactor(none): :recycle: Moved environment.ts
    * Moved environment.ts
    * Added environment.ts to .gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
+import { environment } from './environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
