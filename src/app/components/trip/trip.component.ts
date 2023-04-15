60c66d52512875f5dfcc869feddf91fe9078d4a0
kphutiyagae <kopanophutiyagae@gmail.com>
2023-04-15 15:03:41 +0200

    fix(none): :lipstick: Added Rome.

--- src/app/components/trip/trip.component.ts
+++ src/app/components/trip/trip.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {Store} from "@ngrx/store";
+import {ApiService} from "../../shared/services/api/api.service";
 
 @Component({
   selector: 'app-trip',
@@ -9,7 +11,11 @@ import {ActivatedRoute} from "@angular/router";
 export class TripComponent implements OnInit{
 
   tripId: string | undefined;
-  constructor(private route: ActivatedRoute) {
+  constructor(
+      private route: ActivatedRoute,
+      private store: Store,
+      apiService: ApiService) {
+
   }
   ngOnInit(){
     this.tripId = this.route.snapshot.paramMap.get('tripId') ?? '';
